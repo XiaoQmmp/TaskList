add42c1e26bcd63ece815b0f94aebde37eea3a61
ping-yxq <xiaoqiang13131313@gmail.com>
2022-09-14 23:13:50 +0800

    9.14

---
+++ src/components/MyList.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="todo-main">
+    <MyItem
+      v-for="TodoObj in todos"
+      :key="TodoObj.id"
+      :TodoObj="TodoObj"
+      :chockIodo="chockIodo"
+      :deleteIodo="deleteIodo"
+    />
+  </ul>
+</template>
+
+<script>
+import MyItem from "./MyItem";
+
+export default {
+  name: "MyList",
+  components: {
+    MyItem,
+  },
+  props: ["todos", "chockIodo", "deleteIodo"],
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
